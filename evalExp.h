9e82de36dd57c19688f722ff951b7e7bb37d8341
Lonnekee <36303116+Lonnekee@users.noreply.github.com>
2018-02-17 11:17:50 +0100

    Add files via upload

---
+++ evalExp.h
@@ -0,0 +1,10 @@
+/* evalExp.h, Gerard Renardel, 29 January 2014 */
+
+#ifndef EVALEXP_H
+#define EVALEXP_H
+
+int valueExpression(List *lp, double *wp);
+int valueNumber(List *lp, double *wp);
+void evaluateExpressions();
+
+#endif
